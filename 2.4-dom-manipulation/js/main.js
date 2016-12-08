1f9f09171ebe6610d3e436d451dc27f0aeb0bd9e
Daniel <dray364@gmail.com>
2016-12-08 13:47:11 -0600

    created in the right folder

---
+++ 2.4-dom-manipulation/js/main.js
@@ -0,0 +1,9 @@
+// [x]`addEventListener` accepts TWO arguments:
+// [x]1. What event to listen for
+//   2. What to do when the event happens (callback)
+//   https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
+//
+// document.addEventListener("DOMContentLoaded",function(){
+//
+//
+// })
