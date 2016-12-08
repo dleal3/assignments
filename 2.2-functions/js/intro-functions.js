c301ba1d7fc43a0b6d35cb85630f9371e87c348e
Daniel <dray364@gmail.com>
2016-12-07 21:33:58 -0600

    working on part 3 and discussing some information in the chat room

--- 2.2-functions/js/intro-functions.js
+++ 2.2-functions/js/intro-functions.js
@@ -1,10 +1,10 @@
 
-// HAVE NO CLUE WHAT THIS AREA IS FOR
-var myObject = {
-  superPower: "som text",
-  anotherProperty: "something",
-
-};
+// // HAVE NO CLUE WHAT THIS AREA IS FOR
+// var myObject = {
+//   superPower: "som text",
+//   anotherProperty: "something",
+//
+// };
 
 /**
  * PART 0
@@ -12,58 +12,49 @@ var myObject = {
  * Define a function max() that takes two numbers as arguments
  * and returns the largest of them. Use the if-then-else construct available in Javascript.
  */
-
+ // [x]write a function max()
+ // [x]make two numbers as arguments
+ // [x]return the larger number
+  //[x]use if
+ //[x] use then
+ //[x] use else
 function max(a,b){
   if (a>b){
     return a;
   }else if (b>a) {
     return b;
-// Not really understanding why we need to place another else return b.
-  } else {
+  }else {
     return b;
-
   }
 }
-// So I commented out the formula and tried to rewrite it from memory, but I had to use the reference for the last else return b; statement.
-    // if(a>b){
-    //   return a;
-    // }else if(b>a){
-    //   return b;
-    // }else {
-    //   return b;
-    // }
-
+// console.log(7>4); YESSS I finally got this to work!
 console.assert(max(1,3) === 3);
 console.assert(max(0,3) === 3);
 console.assert(max(10,3) === 10);
 console.assert(max(-1,-3) === -1);
 console.assert(max("aaa",0) === 0);
 console.assert(isNaN(max("aaa","bbb")));
-
-/**
- * PART 1
- *
- * Define a function maxOfThree() that takes three
- * numbers as arguments and returns the largest of them.
- */
-// Trying to write while I read the instructions
-function maxOfThree(a,b,c) {
-  if (a > b && > c){
+//
+// /**
+//  * PART 1
+//  *
+//  * Define a function maxOfThree() that takes three
+//  * numbers as arguments and returns the largest of them.
+//  */
+// [x]Define a function maxOfThree
+//[x]make 3 arguements
+//[x]return larger of them
+//[x] gonna have to ask for a return
+//[x] when we return something we usually have if-then-else
+function maxOfThree(a,b,c){
+  if(a > b && a > c){
     return a;
-  }else if (b > a && > c) {
+  }else if(b > a && b > c ){
     return b;
   }else {
     return c;
   }
 }
-// function maxOfThree(a, b, c){
-//   if( a > b && a > c ){
-//     return a;
-//   }else if( b > a && b > c ){
-//     return b;
-//   }else{return c;}
-
-
 
 console.assert(maxOfThree(1,3,2) === 3);
 console.assert(maxOfThree(0,3,-1) === 3);
@@ -71,36 +62,30 @@ console.assert(maxOfThree(10,3,50) === 50);
 console.assert(maxOfThree(-1,-3,-10) === -1);
 console.assert(maxOfThree("aaa",0,1) === 1);
 console.assert(isNaN(maxOfThree("aaa","bbb","ccc")));
-
-/**
- * PART 2
- *
- * Write a function isVowel() that takes a character (i.e. a string of length 1)
- * and returns true if it is a vowel, false otherwise.
- */
-// Trying to write as I read
-// // function isVowel(char){
-//   var vowelsArray = [
-//     "a","e","i","o","u",
-//     "A", "E", "I", "O", "U"
-//   ]
-//   // Not sure if any of this is right!!
-//     if (isVowel) {
-//       return true;{
-//         else {
-//           return false;
-//         }
-//       }
-//     }
 //
-//   return true;{
-//     if (isVowel) {
-//     }else {
-//       return false;
-//     }
-//   }
+// /**
+//  * PART 2
+//  *
+//  * Write a function isVowel() that takes a character (i.e. a string of length 1)
+//  * and returns true if it is a vowel, false otherwise.
+//  */
+// [x]write a function isVowel()
+// [x]make a char argument (a string of length 1) char.length
+// []return true if it is a vowelsArray
+// []false if it is a consonant
+// [x] have to list vowelsArray
+// []
+// function isVowel(char) {
+//   var vowelsArray = [
+//     "a", "e", "i", "o", "u", "A","E","I","O","U"
+//   ];
+//     if(vowelsArray.indexof(char) === -1){
+//     return false;
+// }else {
+//   return true;
+//  }
 // }
-
+//
 function isVowel(char){
   var vowelsArray = [
     "a","e","i","o","u",
@@ -118,79 +103,79 @@ console.assert(isVowel("B") === false);
 console.assert(isVowel("b") === false);
 console.assert(isVowel("a") === true);
 console.assert(isVowel("E") === true);
-
-/**
- * PART 3
- *
- * Write a function rovarspraket() that will translate
- * a text into a "rövarspråket". That is, double every
- * consonant and place an occurrence of "o" in between.
- *
- * For example, rovarspraket("this is fun") should
- * return the string "tothohisos isos fofunon".
-// THIS IS JUST PLAIN EOEVOVILOL!!!
- */
-
- function rovarspraket(text){
-
- }
-
-// This is where I get lost.
-
-// function rovarspraket(text){
-//   var vowelsArray = [
-//     "a","e","i","o","u",
-//     "A", "E", "I", "O", "U"
-//   ];
-//   for(var i = 0; i < text.length; i++){
-//     var currentLetter = vowelsArray[i];
 //
-//     if(vowelsArray.indexof(char) === -1){
-//       var consonant = char;
-//       var doubledText = "o" + consonant;
+// /**
+//  * PART 3
+//  *
+//  * Write a function rovarspraket() that will translate
+//  * a text into a "rövarspråket". That is, double every
+//  * consonant and place an occurrence of "o" in between.
+//  *
+//  * For example, rovarspraket("this is fun") should
+//  * return the string "tothohisos isos fofunon".
+// // THIS IS JUST PLAIN EOEVOVILOL!!!
+//  */
 //
-//       text.splice(i, 0, doubledText );
-//     }
+//  function rovarspraket(text){
 //
-//   }
-//   return text;
+//  }
+//
+// // This is where I get lost.
+//
+// // function rovarspraket(text){
+// //   var vowelsArray = [
+// //     "a","e","i","o","u",
+// //     "A", "E", "I", "O", "U"
+// //   ];
+// //   for(var i = 0; i < text.length; i++){
+// //     var currentLetter = vowelsArray[i];
+// //
+// //     if(vowelsArray.indexof(char) === -1){
+// //       var consonant = char;
+// //       var doubledText = "o" + consonant;
+// //
+// //       text.splice(i, 0, doubledText );
+// //     }
+// //
+// //   }
+// //   return text;
+// // }
+//
+// console.assert(rovarspraket("a") === "a")
+// console.assert(rovarspraket("b") === "bob")
+// console.assert(rovarspraket("cat") === "cocatot")
+// console.assert(rovarspraket("javascript") === "jojavovasoscocroripoptot")
+// console.assert(rovarspraket(0) === "0")
+//
+// /**
+//  * Part 4
+//  *
+//  * Define a function reverse() that computes
+//  * the reversal of a string. For example,
+//  * reverse("skoob") should return the
+//  * string "books".
+//  */
+//
+// function reverse(str){
+//   // var not sure what the var would be named
 // }
-
-console.assert(rovarspraket("a") === "a")
-console.assert(rovarspraket("b") === "bob")
-console.assert(rovarspraket("cat") === "cocatot")
-console.assert(rovarspraket("javascript") === "jojavovasoscocroripoptot")
-console.assert(rovarspraket(0) === "0")
-
-/**
- * Part 4
- *
- * Define a function reverse() that computes
- * the reversal of a string. For example,
- * reverse("skoob") should return the
- * string "books".
- */
-
-function reverse(str){
-  // var not sure what the var would be named
-}
-
-console.assert(reverse("books") === "skoob")
-console.assert(reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew")
-
-/**
- * Part 5
- *
- * Write a function findLongestWord() that takes an
- * string returns the first, longest word in the array.
- *
- * i.e. findLongestWord("book dogs") should return "book"
- */
-
-function findLongestWord(sentence){
-  // var not sure what the var would be named
-
-}
-
-console.assert(findLongestWord("book dogs") === "book")
-console.assert(findLongestWord("don't mess with Texas") === "Texas")
+//
+// console.assert(reverse("books") === "skoob")
+// console.assert(reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew")
+//
+// /**
+//  * Part 5
+//  *
+//  * Write a function findLongestWord() that takes an
+//  * string returns the first, longest word in the array.
+//  *
+//  * i.e. findLongestWord("book dogs") should return "book"
+//  */
+//
+// function findLongestWord(sentence){
+//   // var not sure what the var would be named
+//
+// }
+//
+// console.assert(findLongestWord("book dogs") === "book")
+// console.assert(findLongestWord("don't mess with Texas") === "Texas")
--- 2.3-loops-and-iterables/loops-iterables.js
+++ 2.3-loops-and-iterables/loops-iterables.js
@@ -34,7 +34,6 @@ function nicer(sentence){
     if(currentWord === "heck" || currentWord === "darn" || currentWord === "crappy" || currentWord === "dang"){
       sentenceArray.splice(i, 1);
     }
-
     // remove from array @ i
     // sentenceArray.splice(i.1)
     // sentenceArray[i] = currentSentence;
@@ -52,48 +51,90 @@ console.assert(nicer("dad get the heck in here and bring me a darn sandwich.") =
 console.assert(nicer("here son, your crappy sandwich is on the dang plate.") === "here son, your sandwich is on the plate.")
 
 // PART 2: write a function called capitalizeAll(). It should take as input a sentence and capitalize the first letter of every word in the sentence.
+// [x] write function named capitalizeAll()
+// [x] going to need a for loop
+// [] going to need a if statement
+// [] going to need to create a var for the input
+// [] going to need to make new sentence with capital letters. (possibly using replace syntax)
+function capitalizeAll(sentence){
+  // since the string is what we need to convert, we use an array.
+  var sentenceArray = sentence.split(" ");
 
+  for (var i = 0; i < sentenceArray.length; i++) {
+
+    var wordArray = sentenceArray[i].split("");
+
+    var capitalizedLetter = wordArray[0].toUpperCase();
+
+    wordArray[0] = capitalizedLetter;
+
+    var joinedWord = wordArray.join("");
+
+    sentenceArray[i] = joinedWord;
+
+  }
+  var joinedSentence = sentenceArray.join(" ")
+  return joinedSentence;
+}
 console.assert(capitalizeAll('every day is like sunday.') === 'Every Day Is Like Sunday.')
 
 // PART 3: write a function called properSentences(). It should take as input a string and capitalize the first letter of every sentence in that string. (For our purposes, all sentences will end with periods. Write one that works with ? and ! and receive a gratifying high five, right on the hand!)
+// [x]write a function properSentenses()
+// [x]make a argument/input
+//[]
+function properSentenses(paragraph){
 
-var paragraph = 'it was a fine morning. the wine was good. light slanted in through the cafe window.'
+  var sentenceArray = sentence.split(".")
 
+  for (var i = 0; i < sentenceArray.length; i++) {
+    var wordArray = sentenceArray[i].split("");
+    wordArray[0] = capitalizedLetter;
+    var joinedWord = wordArray.join(".")
+    sentenceArray[i] = joinedWord;
+}
+
+var joinedSentence = sentencesArray.join("")
+return joinedSentence;
+
+}
+
+// var paragraph = 'it was a fine morning. the wine was good. light slanted in through the cafe window.'
+//
 console.assert(properSentences(paragraph) === "It was a fine morning. The wine was good. Light slanted in through the cafe window.")
 
 // PART 4: write a function called iPutTheFunIn(). It should take a string as input. The output should be a copy of the original string with the word 'fun' inserted into the center of the string.
-
-console.assert(iPutTheFunIn("funerary") === "funefunrary")
-console.assert(iPutTheFunIn("reds") === "refunds")
-
-// PART 5: write a function called pipeline(). it should take three inputs: (1) a starting value, (2) a function, and (3) another function. it should use functions (2) and (3) on the starting value, one after the other, and return a new value that has been processed by both function (2) and function (3).
-
-// the following three tests all correspond to the pipeline() function.
-
-// test 1
-var paragraph = 'dad bring your crappy self in here. i want a dang sandwich.'
-
-console.assert(pipeline(paragraph,nicer,properSentences) === "Mom bring your self in here. I want a sandwich.")
-
-// test 2
-var squareNum = function(n){
-    return n * n
-}
-
-var addOne = function(n) {
-    return n + 1
-}
-
-console.assert(pipeline(7,squareNum,addOne) === 50)
-
-// test 3
-var exclaimAll = function(arr) {
-    var newArr = []
-    for (var i = 0; i < arr.length; i ++) {
-        newArr.push(arr[i] + '!')
-    }
-}
-
-var result = pipeline([10,20,30],squareDance,exclaimAll)
-console.assert(result[1] === "400!")
-console.assert(result[2] === "900!")
+//
+// console.assert(iPutTheFunIn("funerary") === "funefunrary")
+// console.assert(iPutTheFunIn("reds") === "refunds")
+//
+// // PART 5: write a function called pipeline(). it should take three inputs: (1) a starting value, (2) a function, and (3) another function. it should use functions (2) and (3) on the starting value, one after the other, and return a new value that has been processed by both function (2) and function (3).
+//
+// // the following three tests all correspond to the pipeline() function.
+//
+// // test 1
+// var paragraph = 'dad bring your crappy self in here. i want a dang sandwich.'
+//
+// console.assert(pipeline(paragraph,nicer,properSentences) === "Mom bring your self in here. I want a sandwich.")
+//
+// // test 2
+// var squareNum = function(n){
+//     return n * n
+// }
+//
+// var addOne = function(n) {
+//     return n + 1
+// }
+//
+// console.assert(pipeline(7,squareNum,addOne) === 50)
+//
+// // test 3
+// var exclaimAll = function(arr) {
+//     var newArr = []
+//     for (var i = 0; i < arr.length; i ++) {
+//         newArr.push(arr[i] + '!')
+//     }
+// }
+//
+// var result = pipeline([10,20,30],squareDance,exclaimAll)
+// console.assert(result[1] === "400!")
+// console.assert(result[2] === "900!")
