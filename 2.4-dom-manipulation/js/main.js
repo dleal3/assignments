e56b14d88c9f3026403fce10b2709c0c159d713e
Daniel <dray364@gmail.com>
2016-12-09 13:21:50 -0600

    started my js layout

--- 2.4-dom-manipulation/js/main.js
+++ 2.4-dom-manipulation/js/main.js
@@ -1,12 +1,59 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-var cButtonElement = document.querySelector("[data-js='cButton']")
+var cButtonElement = document.querySelector("[data-js='cButton']");
+var outputElement = document.querySelector("[data-js='inputAreaButton']");
+
+// wasn't sure if I needed a var element for the input area that shows
+// var inputAreaButtonElement = document.querySelector("[data-js='inputAreaButton']")
+// var outputElement = document.querySelector("[data-js='inputAreaButton']")
+
+var sevenButtonElement = document.querySelector("[data-js='sevenButton']");
 var outputElement = document.querySelector("[data-js='inputAreaButton']")
 
-var inputAreaButtonElement = document.querySelector("[data-js='inputAreaButton']")
-var outputElement = document.querySelector("[data-js='inputAreaButton']")
+var eightButtonElement = document.querySelector("[data-js='eightButton']");
+var outputElement = document.querySelector("[data-js='inputAreaButton']");
 
+var nineButtonElement = document.querySelector("[data-js='nineButton']");
+var outputElement = document.querySelector("[data-js='inputAreaButton']");
 
+var divideButtonElement = document.querySelector("[data-js='divideButton']");
+var outputElement = document.querySelector("[data-js='inputAreaButton']");
+
+var fourButtonElement = document.querySelector("[data-js='fourButton']");
+var outputElement = document.querySelector("[data-js='inputAreaButton']");
+
+var fiveButtonElement = document.querySelector("[data-js='fiveButton']");
+var outputElement = document.querySelector("[data-js='inputAreaButton']");
+
+var sixButtonElement = document.querySelector("[data-js='sixButton']");
+var outputElement = document.querySelector("[data-js='inputAreaButton']");
+
+var multiplyButtonElement = document.querySelector("[data-js='multiplyButton']");
+var outputElement = document.querySelector("[data-js='inputAreaButton']");
+
+var oneButtonElement = document.querySelector("[data-js='oneButton']");
+var outputElement = document.querySelector("[data-js='inputAreaButton']");
+
+var twoButtonElement = document.querySelector("[data-js='twoButton']");
+var outputElement = document.querySelector("[data-js='inputAreaButton']");
+
+var threeButtonElement = document.querySelector("[data-js='threeButton']");
+var outputElement = document.querySelector("[data-js='inputAreaButton']");
+
+var minusButtonElement = document.querySelector("[data-js='threeButton']");
+var outputElement = document.querySelector("[data-js='inputAreaButton']");
+
+var zeroButtonElement = document.querySelector("[data-js='zeroButton']");
+var outputElement = document.querySelector("[data-js='inputAreaButton']");
+
+var periodButtonElement = document.querySelector("[data-js='periodButton']");
+var outputElement = document.querySelector("[data-js='inputAreaButton']");
+
+var equalButtonElement = document.querySelector("[data-js='equalButton']");
+var outputElement = document.querySelector("[data-js='inputAreaButton']");
+
+var plusButtonElement = document.querySelector("[data-js='plusButton']");
+var outputElement = document.querySelector("[data-js='inputAreaButton']");
 
 
 })
