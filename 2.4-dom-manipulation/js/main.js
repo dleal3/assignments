33a43327115c1936f291096edfd9a51a5237bbe1
Daniel <dray364@gmail.com>
2016-12-09 12:19:33 -0600

    revised the layout of my html, changed class names so that i have one class for numbers, and one for symbol buttons

--- 2.4-dom-manipulation/js/main.js
+++ 2.4-dom-manipulation/js/main.js
@@ -1,3 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+var cButtonElement = document.querySelector("[data-js='cButton']")
+var outputElement = document.querySelector("[data-js='inputAreaButton']")
+
+var inputAreaButtonElement = document.querySelector("[data-js='inputAreaButton']")
+var outputElement = document.querySelector("[data-js='inputAreaButton']")
+
+
+
+
+})
+
 // [x]`addEventListener` accepts TWO arguments:
 // [x]1. What event to listen for
 //   2. What to do when the event happens (callback)
