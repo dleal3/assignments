db3357ef4b6e3c56b1e11866da87e51acad34d9b
Daniel <dray364@gmail.com>
2016-12-12 20:38:59 -0600

    revised he layout of my code and structured it to be more legible

--- 3.1-ajax/js/main.js
+++ 3.1-ajax/js/main.js
@@ -9,63 +9,77 @@ document.addEventListener("DOMContentLoaded", function(){
   xhr.addEventListener("load", function(e){
 
    var xhrData = this.response;
-  //  console.log(xhrData);
-     var JSONData = JSON.parse(xhrData);
-
-     var MovieTitleHTML = "<h2'>";
-     MovieTitleHTML += JSONData.Title;
-     MovieTitleHTML += "</h2>";
-  moviesSectionElement.innerHTML += MovieTitleHTML;
-    var MoviePosterHTML = "<img src='";
-    MoviePosterHTML += JSONData.Poster;
-    MoviePosterHTML += "'>";
-  moviesSectionElement.innerHTML += MoviePosterHTML;
-    var MovieActorsHtml = "<h3>";
-    MovieActorsHtml += "Actors: " + JSONData.Actors;
-    MovieActorsHtml += "</h3>";
-  moviesSectionElement.innerHTML += MovieActorsHtml;
-    var MovieRatedHTML = "<h3>";
-    MovieRatedHTML += "Rated: " + JSONData.Rated;
-    MovieRatedHTML += "</h3>";
-  moviesSectionElement.innerHTML += MovieRatedHTML;
-    var MovieGenreHTML = "<h3 class="font">";
-    MovieGenreHTML += "Genre: " + JSONData.Genre;
-    MovieGenreHTML += "</h3>";
-  moviesSectionElement.innerHTML += MovieGenreHTML;
-
-  // moviesSectionElement.innerHTML += MoviePlotHTML;
-  //   var MoviePlotHTML = "<h3>";
-  //   var MoviePlotHTML = "Plot: " + JSONData.Plot;
-  //   MoviePlotHTML += "</h3>";
-//   var MovieReleaseDateHTML = "<h3>";
-//   MovieReleaseDateHTML += "ReleaseDate: " + JSONData.ReleaseDate;
-//   MovieReleaseDateHTML += "</h3>";
-// moviesSectionElement.innerHTML += MovieReleaseDateHTML;
-  })
-
-  xhr.send();
-})
-
 
   //  var searchArray = JSONData.Search;
-   //
+  //
   //  searchArray.forEach(function(movieData){
-   //
-  //    var movieArticleHTML = "<article>";
-  //    movieArticleHTML += "<h2>";
-  //    movieArticleHTML += movieData.Title;
-  //    movieArticleHTML += "</h2>";
-  //    movieArticleHTML += `<img src='${movieData.Poster}'/>`;
-  //    movieArticleHTML += "<h3>";
-  //    movieArticleHTML += "Release Date: "
-  //    movieArticleHTML += movieData.Year;
-  //    movieArticleHTML += "</h3>";
-  //    movieArticleHTML += "</article>";
-  //    moviesSectionElement.innerHTML += movieArticleHTML;
-   //
+
+    var movieData = JSON.parse(xhrData);
+
+     var movieArticleHTML = "<article>";
+     movieArticleHTML += "<h2>";
+     movieArticleHTML += movieData.Title;
+     movieArticleHTML += "</h2>";
+     movieArticleHTML += `<img src='${movieData.Poster}'/>`;
+     movieArticleHTML += "<h3>";
+     movieArticleHTML += "Release Date: "
+     movieArticleHTML += movieData.Year;
+     movieArticleHTML += "</h3>";
+     movieArticleHTML += "<h3>";
+     movieArticleHTML += "Rated: "
+     movieArticleHTML += movieData.Rated;
+     movieArticleHTML += "</h3>";
+     movieArticleHTML += "<h4>";
+     movieArticleHTML += "Plot: ";
+     movieArticleHTML += movieData.Plot;
+     movieArticleHTML += "</h4>";
+     movieArticleHTML += "<h4>";
+     movieArticleHTML += "Featuring: "
+     movieArticleHTML += movieData.Actors;
+     movieArticleHTML += "</h4>";
+     movieArticleHTML += "</article>";
+     moviesSectionElement.innerHTML += movieArticleHTML;
 
 
- //   });
- // });
- // xhr.send();
-// });
+   })
+
+   xhr.send();
+ })
+
+
+  //  console.log(xhrData);
+//      var JSONData = JSON.parse(xhrData);
+//
+//      var MovieTitleHTML = "<h2'>";
+//      MovieTitleHTML += JSONData.Title;
+//      MovieTitleHTML += "</h2>";
+//   moviesSectionElement.innerHTML += MovieTitleHTML;
+//     var MoviePosterHTML = "<img src='";
+//     MoviePosterHTML += JSONData.Poster;
+//     MoviePosterHTML += "'>";
+//   moviesSectionElement.innerHTML += MoviePosterHTML;
+//     var MovieActorsHtml = "<h3>";
+//     MovieActorsHtml += "Actors: " + JSONData.Actors;
+//     MovieActorsHtml += "</h3>";
+//   moviesSectionElement.innerHTML += MovieActorsHtml;
+//     var MovieRatedHTML = "<h3>";
+//     MovieRatedHTML += "Rated: " + JSONData.Rated;
+//     MovieRatedHTML += "</h3>";
+//   moviesSectionElement.innerHTML += MovieRatedHTML;
+//     var MovieGenreHTML = "<h3 class="font">";
+//     MovieGenreHTML += "Genre: " + JSONData.Genre;
+//     MovieGenreHTML += "</h3>";
+//   moviesSectionElement.innerHTML += MovieGenreHTML;
+//
+//   // moviesSectionElement.innerHTML += MoviePlotHTML;
+//   //   var MoviePlotHTML = "<h3>";
+//   //   var MoviePlotHTML = "Plot: " + JSONData.Plot;
+//   //   MoviePlotHTML += "</h3>";
+// //   var MovieReleaseDateHTML = "<h3>";
+// //   MovieReleaseDateHTML += "ReleaseDate: " + JSONData.ReleaseDate;
+// //   MovieReleaseDateHTML += "</h3>";
+// // moviesSectionElement.innerHTML += MovieReleaseDateHTML;
+//   })
+//
+//   xhr.send();
+// })
