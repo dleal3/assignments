663eb99fe007b6d7aecd823f399e24e59116c991
Daniel <dray364@gmail.com>
2016-12-14 14:53:26 -0600

    added content info for David, Sharice, Lucio and Nicholas

--- 3.3-accordion/js/main.js
+++ 3.3-accordion/js/main.js
@@ -4,12 +4,23 @@ $(function(){
   // We must put all DOM related code inside of this object.
 
   // We need to make a variable to reference the students
-  var $studentElements = $("[data-jsj='student']");
+  var $studentElements = $("[data-js='student']");
 
   // Now we need to give the student reference a listener
   $studentElements.on("click", function(e){
     // Now we need to find a reference to the element that was clicked by using 'this' keyword
     var $clickedStudent = $(this);
-  })
 
-})
+    // Now we must remove the element that was clicked by using the .remove() method to remove the clicked element
+    // $clickedStudent.remove();
+
+    // We need to remove the class of expand from the one that already has the class of expand
+    $studentElements.filter(".expand").removeClass("expand");
+
+
+    // Need to expand element to be taller
+    // We give the clicked student the class of expand in html
+    $clickedStudent.addClass("expand");
+  });
+
+});
