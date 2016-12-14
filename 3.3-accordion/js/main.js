ee247aa050f953877ca3271e619793e33a0e6aa2
Daniel <dray364@gmail.com>
2016-12-14 13:42:02 -0600

    made a variable reference to the students and also gave the student reference a listener

--- 3.3-accordion/js/main.js
+++ 3.3-accordion/js/main.js
@@ -4,6 +4,12 @@ $(function(){
   // We must put all DOM related code inside of this object.
 
   // We need to make a variable to reference the students
-  var $studentElements = $("[data-jsj='student']")
+  var $studentElements = $("[data-jsj='student']");
+
+  // Now we need to give the student reference a listener
+  $studentElements.on("click", function(e){
+    // Now we need to find a reference to the element that was clicked by using 'this' keyword
+    var $clickedStudent = $(this);
+  })
 
 })
