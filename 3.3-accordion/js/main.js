b4ec829f38720ba4f8b44c5e507bac492496bc18
Daniel <dray364@gmail.com>
2016-12-14 13:37:20 -0600

    made a jQuery factory call in JS and gave it an anonymous function

---
+++ 3.3-accordion/js/main.js
@@ -0,0 +1,9 @@
+// We need a jQuery factory call
+  // We also need to give it an anonymous function so we can wait for page content to load
+$(function(){
+  // We must put all DOM related code inside of this object.
+
+  // We need to make a variable to reference the students
+  var $studentElements = $("[data-jsj='student']")
+
+})
