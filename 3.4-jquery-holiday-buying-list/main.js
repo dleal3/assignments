d5a1966cc48f41e2f739ac3145b7f6c0a20816dc
Daniel <dray364@gmail.com>
2016-12-15 15:49:52 -0600

    just pushing up because I had unstaged changes

---
+++ 3.4-jquery-holiday-buying-list/main.js
@@ -0,0 +1,28 @@
+$(function(){
+  var fafa_check_circle_oURL = "http://fontawesome.io/icon/check-circle-o/"
+
+  // this is all code related to DOM
+  var $body = $("body");
+
+  var $textarea = $body.find("[data-js='shoppingListForm__text']");
+  var $submitButton = $body.find("[data-js='shoppingListText__submit']");
+  var $shoppingListPostsElement = $body.find("[data-js='shoppingListPosts']");
+
+  console.log($submitButton);
+  $submitButton.on("click", function(e){
+
+    e.preventDefault();
+
+    var textAreaValue = $textarea.val();
+
+    var $paragraphElement = $("<p>").text(textAreaValue);
+
+    var $articleHTML = $("<article>")
+      .append($paragraphElement);
+
+      $shoppingListPostsElement.prepend($articleHTML)
+
+
+  });
+
+});
