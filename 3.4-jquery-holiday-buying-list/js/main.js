9cd9a19c5dcb20e62326e4fe6054335660a94d22
Daniel <dray364@gmail.com>
2016-12-19 00:56:49 -0600

    created var to capture text input/ created var elements for data-js things / created new list items using jquery which is the dopeness

--- 3.4-jquery-holiday-buying-list/js/main.js
+++ 3.4-jquery-holiday-buying-list/js/main.js
@@ -3,10 +3,10 @@
 // [x] We need to create vars to reference html elements
 // [x] We need to add data-js to connect to html
 // [x] We need to on click for the add button
-// [x] We need to add the preventDefault 
-// []
-// []
-// []
+// [x] We need to add the preventDefault
+// [x] We need to make a var to capture the text input
+// [x] We need to create var elements for data-js things
+// [x] Created a new list item using JQUERY! made a var for button and article lines 25 and 32
 // []
 $(function(){
   var fafa_check_circle_oURL = "http://fontawesome.io/icon/check-circle-o/"
@@ -18,10 +18,52 @@ $(function(){
   var totalElement = $body.find("[data-js='total']");
   $addButton.on("click", function(e){
     e.preventDefault();
-
-  })
-
-
+    var textInputValue = $text.val();
+    var textInputArray = textInputValue.split(",");
+    var listTextElement = textInputArray[0];
+    var priceElement = textInputArray[1];
+    var $circleButton = $("<button>") // created a var for circle button
+      .attr({ // here we are going to create a circle button in jquery.. WHICH IS EFFIN AWESOME!
+        class: "circleButton",
+        type: "button",
+        value: "line",
+        "data-js": "circle"
+      });
+    var $listTextElement = $("<p>")
+      .attr({
+        class: "listItem__text",
+        "data-js": "itemName"
+      })
+      .text(listTextElement);
+    var $priceElement = $("<p>")
+      .attr({
+        class: "priceElement",
+        "data-js" : "price",
+        value: priceElement
+      })
+      .text("$"+(priceElement))
+      var $articleHTML = $("<article>")
+        .attr({
+          class: "listItem"
+        })
+      .append($circleButton)
+      .append($listTextElement)
+      .append($priceElement)
+    $articleElement.prepend($articleHTML);
+    var totalString = "";
+    var totalPrice = "";
+    $priceElement.each{function(priceElement){
+      var priceElement = $(".priceElement");
+      totalString += priceElement.text();
+      var input = totalString;
+      var numbers = input.split("$");
+      var numberString = numbers.join("+");
+      var totalOfEval = eval(numberString)
+      totalPrice = totalOfEval;
+    }};
+    $totalElement.text{totalPrice};
+  });
+    
 
   // this is all code related to DOM
   // var $body = $("body");
