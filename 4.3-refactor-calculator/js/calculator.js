5d444531d9b42e0eac3a89b6e26abf44c9019083
Daniel <dray364@gmail.com>
2016-12-21 17:08:49 -0600

    pushing up recent changes

--- 4.3-refactor-calculator/js/calculator.js
+++ 4.3-refactor-calculator/js/calculator.js
@@ -1,3 +1,4 @@
+var $ = require("jquery");
 $(function(){
   // Everything in here will be able to see content / after it loads
 var $cButtonElement = $("[data-js='cButton']");
@@ -5,99 +6,121 @@ var $outputElement = $("[data-js='inputAreaButton']");
 $cButtonElement.on("click", function(){
     $outputElement.textContent = "";
 });
-// wasn't sure if I needed a var element for the input area that shows
-// var inputAreaButtonElement = document.querySelector("[data-js='inputAreaButton']")
-// var outputElement = document.querySelector("[data-js='inputAreaButton']")
-
+// });
 var $sevenButtonElement = $("[data-js='sevenButton']");
 var $outputElement = $("[data-js='inputAreaButton']");
 $sevenButtonElement.on("click", function(){
   $outputElement.textContent += 7;
 });
 
+//
+// //
 var $eightButtonElement = $("[data-js='eightButton']");
 var $outputElement = $("[data-js='inputAreaButton']");
 $eightButtonElement.on("click", function(){
   $outputElement.textContent += 8;
 });
 
+//
 var $nineButtonElement = $("[data-js='nineButton']");
 var $outputElement = $("[data-js='inputAreaButton']");
 $nineButtonElement.on("click", function(){
   $outputElement.textContent += 9;
 });
 
+//
 var $divideButtonElement = $("[data-js='divideButton']");
 var $outputElement = $("[data-js='inputAreaButton']");
 $divideButtonElement.on("click", function(){
   $outputElement.textContent += "/"
-})
-
+});
+//
 var $fourButtonElement = $("[data-js='fourButton']");
 var $outputElement = $("[data-js='inputAreaButton']");
 $fourButtonElement.on("click", function(){
   $outputElement.textContent += 4;
 });
+
+//
 var $fiveButtonElement = $("[data-js='fiveButton']");
 var $outputElement = $("[data-js='inputAreaButton']");
 $fiveButtonElement.on("click", function(){
   $outputElement.textContent += 5;
 });
+
+//
 var $sixButtonElement = $("[data-js='sixButton']");
 var $outputElement = $("[data-js='inputAreaButton']");
 $sixButtonElement.on("click", function(){
   $outputElement.textContent += 6;
 });
+
+//
 var $multiplyButtonElement = $("[data-js='multiplyButton']");
 var $outputElement = $("[data-js='inputAreaButton']");
 $multiplyButtonElement.on("click", function(){
   $outputElement.textContent += "*";
 });
 
+//
+// //
 var $oneButtonElement = $("[data-js='oneButton']");
 var $outputElement = $("[data-js='inputAreaButton']");
 $oneButtonElement.on("click", function(){
   $outputElement.textContent += 1;
 });
-var twoButtonElement = document.querySelector("[data-js='twoButton']");
-var outputElement = document.querySelector("[data-js='inputAreaButton']");
-twoButtonElement.addEventListener("click", function(){
-  outputElement.textContent += 2;
+//
+var $twoButtonElement = $("[data-js='twoButton']");
+var $outputElement = $("[data-js='inputAreaButton']");
+$twoButtonElement.on("click", function(){
+  $outputElement.textContent += 2;
 });
-var threeButtonElement = document.querySelector("[data-js='threeButton']");
-var outputElement = document.querySelector("[data-js='inputAreaButton']");
-threeButtonElement.addEventListener("click", function(){
-  outputElement.textContent += 3;
-});
-
-var minusButtonElement = document.querySelector("[data-js='minusButton']");
-var outputElement = document.querySelector("[data-js='inputAreaButton']");
-minusButtonElement.addEventListener("click", function(){
-  outputElement.textContent += "-";
-});
-var zeroButtonElement = document.querySelector("[data-js='zeroButton']");
-var outputElement = document.querySelector("[data-js='inputAreaButton']");
-zeroButtonElement.addEventListener("click", function(){
-  outputElement.textContent += 0;
+//
+//
+var $threeButtonElement = $("[data-js='threeButton']");
+var $outputElement = $("[data-js='inputAreaButton']");
+$threeButtonElement.on("click", function(){
+  $outputElement.textContent += 3;
 });
 
-var periodButtonElement = document.querySelector("[data-js='periodButton']");
-var outputElement = document.querySelector("[data-js='inputAreaButton']");
-periodButtonElement.addEventListener("click", function(){
-  outputElement.textContent += ".";
+//
+var $minusButtonElement = $("[data-js='minusButton']");
+var $outputElement = $("[data-js='inputAreaButton']");
+$minusButtonElement.on("click", function(){
+  $outputElement.textContent += "-";
 });
 
-var equalButtonElement = document.querySelector("[data-js='equalButton']");
-var outputElement = document.querySelector("[data-js='inputAreaButton']");
-equalButtonElement.addEventListener("click", function(){
-  var answerOutput = eval(outputElement.textContent);
-  outputElement.textContent = answerOutput;
+//
+var $zeroButtonElement = $("[data-js='zeroButton']");
+var outputElement = $("[data-js='inputAreaButton']");
+$zeroButtonElement.on("click", function(){
+  $outputElement.textContent += 0;
 });
 
-var plusButtonElement = document.querySelector("[data-js='plusButton']");
-var outputElement = document.querySelector("[data-js='inputAreaButton']");
-plusButtonElement.addEventListener("click", function(){
-  outputElement.textContent += "+";
+//
+// //
+var $periodButtonElement = $("[data-js='periodButton']");
+var $outputElement = $("[data-js='inputAreaButton']");
+$periodButtonElement.on("click", function(){
+  $outputElement.textContent += ".";
 });
 
+//
+// //
+var $equalButtonElement = $("[data-js='equalButton']");
+var $outputElement = $("[data-js='inputAreaButton']");
+$equalButtonElement.on("click", function(){
+  var $answerOutput = eval($outputElement.textContent);
+  $outputElement.textContent = $answerOutput;
+});
+//
+// //
+var $plusButtonElement = $("[data-js='plusButton']");
+var $outputElement = $("[data-js='inputAreaButton']");
+$plusButtonElement.on("click", function(){
+  $outputElement.textContent += "+";
+});
+console.log("this is a textlskfjlsdfj;afldj");
+
+//
 });
