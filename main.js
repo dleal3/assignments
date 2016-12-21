db778c62493f7ac871a49840396bf296278d6624
Daniel <dray364@gmail.com>
2016-12-21 00:28:31 -0600

    laid out the basic structure of the javascript / now just need to work on getting the data-js labeled

---
+++ main.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <script src="dist/bundle.js" charset="utf-8"></script>
+    <link rel="stylesheet" href="styles/main.css">
+  </head>
+  <body>
+
+  </body>
+</html>
