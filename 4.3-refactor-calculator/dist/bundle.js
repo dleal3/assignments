311843549ed9a31db694a1d186fc9bf5ffd6ec6b
Daniel <dray364@gmail.com>
2016-12-24 11:40:52 -0600

    Referred to Justin's code and tried to edit my html classes to follow the structure that he followed/ I also referred to his js code and see how he wrote out his js, but at the time being, my calculator is not showing any output still

--- 4.3-refactor-calculator/dist/bundle.js
+++ 4.3-refactor-calculator/dist/bundle.js
@@ -69,44 +69,21 @@
 	// [x] need to create var for symbolButtonElement
 	// [x] create var for outputElement
 	// [x] Rename html buttons to just one data-js class called Button (this provides shorter code for js that allows each button to do it's job and send it in the output)
-	// [x] Need to make a variable for the equalButton that has a data-js class called equalButton
+	// [x] Need to make a variable for the equalButton that has a data-js class called numberKeyEqual
 	var $ = __webpack_require__(2);
+
 	$(function(){
-	  var $body = $("body");
+	  // need to declare a var for body and assign it a value
+	var $body = $(".calc__body");
+	var $numberKeyElementClear = $body.find("[data-js='numberKeyClear']");
+	var $numberKeyElementEqual = $body.find("[data-js='numberKeyEqual']");
+	var $outputWindowElement = $body.find("[data-js='output__Window']");
 
-	  var $ButtonElement = $body.find("[data-js='Button']");
+	$numberKeyElementEqual.on("click", function (e){
+	  var answerOutput = eval($outputWindowElement.html());
+	  $outputWindowElement.html(answerOutput);
+	});
 
-	  var $outputElement = $body.find("[data-js='output']");
-
-	  var $equalButtonElement = $body.find("[data-js='equalButton']")
-
-
-
-	// need to make a jquery function for the ButtonElement which will have a click listener
-	// create a var for when button is clicked (clickedButtonElement) and set it to $(this)
-	// create a var for number that will display the clicked button
-	// set the jquery outputElement
-	  $ButtonElement.on("click", function(e){
-
-	    var $clickedButtonElement = $(this);
-
-	    var currentOutputString = $outputElement.text();
-
-	    var number = $clickedButtonElement.text();
-	// we are concatinating the currentOutputString and the number to show in the output
-	    $outputElement.text(currentOutputString + number);
-	  });
-
-
-
-
-	  $equalButtonElement.on("click", function(e){
-
-	    var answer = "";
-	      answer = eval($outputElement.html());
-	      $outputElement.html(answer);
-
-	    });
 	});
 
 
