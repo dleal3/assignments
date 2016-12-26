319750c35079cd70d7bb1a1dccd8c49e68696f65
Daniel <dray364@gmail.com>
2016-12-26 00:14:32 -0600

    FINALLY HAVE a photo grid with an aside section of button / styled the images and aligned buttons

--- 4.1-photos-structure/js/main.js
+++ 4.1-photos-structure/js/main.js
@@ -1,18 +1,18 @@
-var $modalClose = $body.find("[data-js='modal__close']");
-  var $mainElement = $body.find("[data-js='main']");
-  var $imgElement = $body.find("[data-js='img']");
-
-  var $backButton = $body.find("[data-js='']");
-  var $albumHeading2 = $body.find("[data-js='']");
-
-    $imgElement.on("click", function(e){
-      $albumHeading2.addClass("");
-      $mainElement.addClass("");
-      $modalClose.removeClass("");
-  });
-
-    $backButton.on("click", function(e){
-      $albumHeading2.removeClass("");
-      $mainElement.removeClass("");
-      $modalClose.addClass("");
-  });
+// var $modalClose = $body.find("[data-js='modal__close']");
+//   var $mainElement = $body.find("[data-js='main']");
+//   var $imgElement = $body.find("[data-js='img']");
+//
+//   var $backButton = $body.find("[data-js='']");
+//   var $albumHeading2 = $body.find("[data-js='']");
+//
+//     $imgElement.on("click", function(e){
+//       $albumHeading2.addClass("");
+//       $mainElement.addClass("");
+//       $modalClose.removeClass("");
+//   });
+//
+//     $backButton.on("click", function(e){
+//       $albumHeading2.removeClass("");
+//       $mainElement.removeClass("");
+//       $modalClose.addClass("");
+//   });
