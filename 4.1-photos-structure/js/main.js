280db395f72cf656fb2fce8b06a2e43e6884fb23
Daniel <dray364@gmail.com>
2016-12-26 00:56:38 -0600

    added data-js classes to each photo in html / added article and photo for modal image / started writing the js for the main js /

--- 4.1-photos-structure/js/main.js
+++ 4.1-photos-structure/js/main.js
@@ -1,18 +1,37 @@
-// var $modalClose = $body.find("[data-js='modal__close']");
-//   var $mainElement = $body.find("[data-js='main']");
-//   var $imgElement = $body.find("[data-js='img']");
-//
-//   var $backButton = $body.find("[data-js='']");
-//   var $albumHeading2 = $body.find("[data-js='']");
-//
-//     $imgElement.on("click", function(e){
-//       $albumHeading2.addClass("");
-//       $mainElement.addClass("");
-//       $modalClose.removeClass("");
-//   });
-//
-//     $backButton.on("click", function(e){
-//       $albumHeading2.removeClass("");
-//       $mainElement.removeClass("");
-//       $modalClose.addClass("");
-//   });
+var $ = require ("jquery");
+$(function(){
+
+  var $body = $("body");
+
+  var $photo1 = $body.find("[data-js='photo1']");
+  var $photo2 = $body.find("[data-js='photo2']");
+  var $photo3 = $body.find("[data-js='photo3']");
+  var $photo4 = $body.find("[data-js='photo4']");
+  var $photo5 = $body.find("[data-js='photo5']");
+  var $photo6 = $body.find("[data-js='photo6']");
+
+  var $backButtonElement = ("[data-js='backButton']");
+
+  var #modalElement = ("[data-js='modal']");
+
+  
+
+
+})
+  // var $mainElement = $body.find("[data-js='main']");
+  // var $imgElement = $body.find("[data-js='image']");
+  //
+  // var $backButton = $body.find("[data-js='']");
+  // var $albumHeading2 = $body.find("[data-js='']");
+  //
+  //   $imgElement.on("click", function(e){
+  //     $albumHeading2.addClass("");
+  //     $mainElement.addClass("");
+  //     $modalClose.removeClass("");
+  // });
+  //
+  //   $backButton.on("click", function(e){
+  //     $albumHeading2.removeClass("");
+  //     $mainElement.removeClass("");
+  //     $modalClose.addClass("");
+  // });
