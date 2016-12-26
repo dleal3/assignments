8c155b4de1f502d7589d838117630f9f97405337
Daniel <dray364@gmail.com>
2016-12-26 02:10:52 -0600

    trying to get the modal feature to function, but not working at all... gonna keep trying to find a solution

--- 4.1-photos-structure/js/main.js
+++ 4.1-photos-structure/js/main.js
@@ -3,21 +3,40 @@ $(function(){
 
   var $body = $("body");
 
-  var $photo1 = $body.find("[data-js='photo1']");
-  var $photo2 = $body.find("[data-js='photo2']");
-  var $photo3 = $body.find("[data-js='photo3']");
-  var $photo4 = $body.find("[data-js='photo4']");
-  var $photo5 = $body.find("[data-js='photo5']");
-  var $photo6 = $body.find("[data-js='photo6']");
+  var modal = document.getElementByID('myModal');
 
-  var $backButtonElement = ("[data-js='backButton']");
+  var btn = document.getElementByID('myBtn');
 
-  var #modalElement = ("[data-js='modal']");
-
-  
+  var JordanOne = document.getElementByID('myJordan');
 
 
-})
+  btn.onclick = function(){
+    modal.style.display = "block";
+  }
+
+  JordanOne.onclick = function(){
+    modal.style.display = "none";
+  }
+  // var $photo1 = $body.find("[data-js='photo1']");
+  // var $photo2 = $body.find("[data-js='photo2']");
+  // var $photo3 = $body.find("[data-js='photo3']");
+  // var $photo4 = $body.find("[data-js='photo4']");
+  // var $photo5 = $body.find("[data-js='photo5']");
+  // var $photo6 = $body.find("[data-js='photo6']");
+  //
+  // var $backButtonElement = ("[data-js='backButton']");
+  //
+  // var #modalElement = ("[data-js='modal']");
+  //
+  // $photo1.on("click", function(e){
+  //   $modalElement.removeClass("hidden");
+  // });
+  //
+  // $backButtonElement.on("click", function(e){
+  //   $modalElement.addClass("hidden");
+  // });
+
+});
   // var $mainElement = $body.find("[data-js='main']");
   // var $imgElement = $body.find("[data-js='image']");
   //
