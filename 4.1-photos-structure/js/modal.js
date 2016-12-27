de920500e9afbf9fb9b37cbf9afbf5626213d313
Daniel <dray364@gmail.com>
2016-12-27 14:40:40 -0600

    created var for clickedImageSrc and assigned it to return the current clicked element and the .attr returns the value of whatever attribute we pass it / in this case we pass the 'src' attribute

--- 4.1-photos-structure/js/modal.js
+++ 4.1-photos-structure/js/modal.js
@@ -14,44 +14,58 @@ var $photo3 = $body.find("[data-js='photo3']");
 var $photo4 = $body.find("[data-js='photo4']");
 var $photo5 = $body.find("[data-js='photo5']");
 var $photo6 = $body.find("[data-js='photo6']");
+var $modalImage = $body.find("[data-js='modalImage']");
+
 
    // Add a click event listener on photo1 element
   $photo1.on("click", function(e){
+    var $clickedImageSrc = $(e.currentTarget).attr("src");
     // Inside of click event open modal
     $modal.removeClass("hide");
-    $albumPage.addClass("hide");
+    $modalImage.attr("src", $clickedImageSrc);
   });
 
   // This click event for the backbutton closes the modal and returns to album page
   $backButton.on("click", function(e){
     $modal.addClass("hide");
-    $albumPage.addClass("hide");
-      });
+
   });
 
+  $photo2.on("click", function(e){
+    var $clickedImageSrc = $(e.currentTarget).attr("src");
+    // Inside of click event open modal
+    $modal.removeClass("hide");
+    $modalImage.attr("src", $clickedImageSrc);
+  });
 
+  $photo3.on("click", function(e){
+    var $clickedImageSrc = $(e.currentTarget).attr("src");
+    // Inside of click event open modal
+    $modal.removeClass("hide");
+    $modalImage.attr("src", $clickedImageSrc);
 
-  // $photo2.on("click", function(e){
-  //   $modal.addClass("hide");
-  //   $albumPage.addClass("hide");
-  // });
-  //
-  // $photo3.on("click", function(e){
-  //   $modal.addClass("hide");
-  //   $albumPage.addClass("hide");
-  // });
-  //
-  // $photo4.on("click", function(e){
-  //   $modal.addClass("hide");
-  //   $albumPage.addClass("hide");
-  // });
-  //
-  // $photo5.on("click", function(e){
-  //   $modal.addClass("hide");
-  //   $albumPage.addClass("hide");
-  // });
-  //
-  // $photo6.on("click", function(e){
-  //   $modal.addClass("hide");
-  //   $albumPage.addClass("hide");
-  // });
+  });
+
+  $photo4.on("click", function(e){
+    var $clickedImageSrc = $(e.currentTarget).attr("src");
+    // Inside of click event open modal
+    $modal.removeClass("hide");
+    $modalImage.attr("src", $clickedImageSrc);
+
+  });
+  $photo5.on("click", function(e){
+    var $clickedImageSrc = $(e.currentTarget).attr("src");
+    // Inside of click event open modal
+    $modal.removeClass("hide");
+    $modalImage.attr("src", $clickedImageSrc);
+
+  });
+  $photo6.on("click", function(e){
+    var $clickedImageSrc = $(e.currentTarget).attr("src");
+    // Inside of click event open modal
+    $modal.removeClass("hide");
+    $modalImage.attr("src", $clickedImageSrc);
+
+  });
+  // closes the entire jquery function
+});
