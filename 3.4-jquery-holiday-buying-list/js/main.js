5bea31ee5bff58d465df5d776dba3ec4eba4b858
Daniel <dray364@gmail.com>
2016-12-28 02:07:12 -0600

    FINALLY have a working shopping list / Used Davids js code as reference that really helped with functionality

--- 3.4-jquery-holiday-buying-list/js/main.js
+++ 3.4-jquery-holiday-buying-list/js/main.js
@@ -1,3 +1,6 @@
+// FOR THIS PROJECT I STARTED FROM SCRATCH AND USED DAVIDS CODE AS REFERENCE 
+
+
 // [x] We need to factory call the document to start
 // [x] We need to factory call the $body and $text
 // [x] We need to create vars to reference html elements
@@ -7,7 +10,6 @@
 // [x] We need to make a var to capture the text input
 // [x] We need to create var elements for data-js things
 // [x] Created a new list item using JQUERY! made a var for button and article lines 25 and 32
-// []
 $(function(){
   var fafa_check_circle_oURL = "http://fontawesome.io/icon/check-circle-o/"
   var $body = $("body");
@@ -35,4 +37,56 @@ $(function(){
         "data-js": "itemName"
       })
       .text(listTextElement);
+    var $priceElement = $("<p>")
+      .attr({
+        class: "priceElement",
+        "data-js": "price",
+        value: priceElement
+      })
+      .text("$"+(priceElement));
+    var $articleHTML = $("<article>")
+      .attr({
+        class: "listItem"
+      })
+      .append($circleButton)
+      .append($listTextElement)
+      .append($priceElement)
+    $articleElement.prepend($articleHTML);
+    var totalString = "";
+    var totalPrice = "";
+    $priceElement.each(function(priceElement){
+      var priceElement = $(".priceElement");
+      totalString += priceElement.text();
+      var input = totalString;
+      var numbers = input.split("$");
+      var numberString = numbers.join("+");
+      var totalOfEval = eval(numberString)
+      totalPrice = totalOfEval;
+
+    });
+    totalElement.text(totalPrice);
   });
+  $body.on("click", "[data-js='circle']", function(){
+    var $this = $(this);
+    $this.siblings().addClass("lineThrough");
+    $this.html("&#10003");
+    var siblingsArray = $this.siblings();
+    var pPriceElement = siblingsArray[1];
+    var currentPriceToSubtract = $(pPriceElement).attr("value");
+    var newSubtractedTotal = $totalElement.text() - currentPriceToSubtract;
+    $totalElement.text(newSubtractedTotal);
+  });
+  $body.on("dblclick", "[data-js='itemName']", function(){
+    var $this = $(this);
+    $this.get(0).contentEditable = "true";
+  });
+  $body.on("dblclick", "[data-js='price']", function(){
+    var $this = $(this);
+    $this.get(0).contentEditable = "true";
+  });
+
+  $body.on("dblclick", "[data-js='total']", function(){
+    var $this = $(this);
+    $this.get(0).contentEditable = "true";
+ });
+});
