e959c2971d75cafa93d3dcd515fd87f1c08eead4
Daniel <dray364@gmail.com>
2017-01-05 14:32:32 -0600

    adding to my 5.3 html so that I can start seeing the GIFT TO BUY h2

---
+++ 5.3-gift-react/js/main.js
@@ -0,0 +1,18 @@
+// Grabbing react from node_modules
+import React from 'react'
+
+export default React.createClass({
+  getInitialState() {
+    return {
+      output: "enter gift item here"
+    }
+  },
+  render() {
+    return (
+      <form>
+        <input></input>
+      </form>
+    )
+  }
+
+});
