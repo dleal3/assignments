18fd41d3de3760141b3d31aa5f71fc4db27d1ced
Daniel <dray364@gmail.com>
2017-01-05 13:43:18 -0600

    installed react/babel and added files needed for our boilerplate

--- boilerplate-jasmine/webpack.config.js
+++ boilerplate-jasmine/webpack.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-  entry: "./js/main.js",
+  entry: "./js/index.js",
   devtool: "source-map",
   output: {
-    path: "./dist",
+    path: "dist",
     filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+    ]
   }
 }
