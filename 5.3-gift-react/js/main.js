4a4e5abea7bd75498000f5eb37ac52c3a49f4bad
Daniel <dray364@gmail.com>
2017-01-05 16:42:54 -0600

    laid out the render portion of main.js and used html as a reference

--- 5.3-gift-react/js/main.js
+++ 5.3-gift-react/js/main.js
@@ -9,10 +9,22 @@ export default React.createClass({
   },
   render() {
     return (
-      <form>
-        <input></input>
-        <input></input>
-      </form>
+    <section>
+      <h2> Gifts To Buy </h2>
+      <main>
+        <form>
+          <input></input><input></input>
+          <div>
+            <article></article>
+            <article>
+              <p>Total</p>
+              <p>$</p>
+              <p>0</p>
+            </article>
+          </div>
+        </form>
+      </main>
+    </section>
     )
   }
 
