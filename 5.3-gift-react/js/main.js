71c97199a41251701b168f08a14e5df51cd0f118
Daniel <dray364@gmail.com>
2017-01-05 21:45:03 -0600

    added  classNames in index.js to elements and styled them in main.css

--- 5.3-gift-react/js/main.js
+++ 5.3-gift-react/js/main.js
@@ -5,21 +5,24 @@ export default React.createClass({
   getInitialState() {
     return {
       output: "enter gift item here"
-    }
+    };
   },
+
+
   render() {
     return (
-    <section>
-      <h2> Gifts To Buy </h2>
+    <section className="section">
+      <h2 className= "h2" > Gifts To Buy </h2>
       <main>
         <form>
-          <input></input><input></input>
-          <div>
-            <article></article>
-            <article>
-              <p>Total</p>
-              <p>$</p>
-              <p>0</p>
+          <input placeholder= "Enter Text Here" className= "giftInputText" value={ this.state.inputValue } onChange={this.updateInputValue}></input>
+          <input placeholder= "ADD" className= "addButton" onClick={this.onAddButtonClick}></input>
+          <div className="listItems_div">
+            <article className="listItem"></article>
+            <article className="totalAmount">
+              <p className="totalTextOnly">Total</p>
+              <p className="p">$</p>
+              <p className="p__totalCost">0</p>
             </article>
           </div>
         </form>
