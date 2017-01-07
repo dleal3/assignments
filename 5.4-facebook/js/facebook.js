ef92924694fd5486265bf3b03b702c017a337cb2
Daniel <dray364@gmail.com>
2017-01-07 11:25:42 -0600

    renamed facebook.js file

--- 5.4-facebook/js/facebook.js
+++ 5.4-facebook/js/facebook.js
@@ -1,2 +1,16 @@
 // Grabbing react from node_modules
 import React from 'react'
+
+export default React.createClass({
+  getInitialState(){
+    return {
+      // FILL THIS SECTION OUT WHEN WE WANT TO RETURN FB COMMENTS
+    }
+  },
+
+  render() {
+    return (
+      
+    )
+  }
+})
