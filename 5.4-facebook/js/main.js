a3cc8e56a2257efeeb7da310308e0773dc6e053e
Daniel <dray364@gmail.com>
2017-01-08 21:18:40 -0600

    profile img/ profile link/ post date/ post contnt

--- 5.4-facebook/js/main.js
+++ 5.4-facebook/js/main.js
@@ -11,11 +11,11 @@ export default React.createClass({
 
   render() {
     return (
-      <section>
-      <img></img>
-      <h2> Instant Articles </h2>
-      <h3> May 13, 2015 </h3>
-      <p> LOREM IPSUM </p>
+      <section className="post__container">
+        <img src="https://avatars1.githubusercontent.com/u/23640802?v=3&s=40" className="profile__image"></img>
+        <h2 className="post__title"> <a href=""> Instant Articles </a></h2>
+        <h3 className="post__date"> May 13, 2015 </h3>
+      <p className=""> LOREM IPSUM </p>
       </section>
     )
   }
