b73c32b1c5e722c49e0f2312cdbeb6e55a044ba1
Daniel <dray364@gmail.com>
2017-01-08 21:36:21 -0600

    starting to look alot like facebook article / a nothing whatsoever like xmas

--- 5.4-facebook/js/main.js
+++ 5.4-facebook/js/main.js
@@ -15,7 +15,9 @@ export default React.createClass({
         <img src="https://avatars1.githubusercontent.com/u/23640802?v=3&s=40" className="profile__image"></img>
         <h2 className="post__title"> <a href=""> Instant Articles </a></h2>
         <h3 className="post__date"> May 13, 2015 </h3>
-      <p className=""> LOREM IPSUM </p>
+      <p className=""> Welcome! To see these stories in the Instant Articles format, you must be on the latest version of the Facebook app for iPhone or Android.</p>
+      <p> Learn more about this new tool for publishers at <a href=""> instantarticles.fb.com </a> </p>
+      <p className="post__likes"> 778 Likes  315 Comments  53 Shares </p>
       </section>
     )
   }
