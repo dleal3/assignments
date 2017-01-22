eabf44d6aedb698386dfcc1750826fc24684f136
Daniel <dray364@gmail.com>
2017-01-22 17:35:48 -0600

    added form and inputs in main.js file

--- 7.4-chat-app/js/main.js
+++ 7.4-chat-app/js/main.js
@@ -1,1 +1,33 @@
 import React from 'react'
+import { ajax } from 'jquery'
+
+export default React.createClass({
+
+  getInitialState (){
+    return {
+      userNames: "",
+      chatMessages: "",
+      chatJsonData: []
+    }
+  },
+
+  render (){
+    <section className="chatAppBody">
+      <div className="chatMessagesDisplay">
+        {
+          this.state.chatJsonData.map((currentPost, i)=>{
+            return <p key={i} className="currentPost"> <span className="userNameStyle" >{currentPost.currentUser}</span> <span className="userMessageStyle">{currentPost.currentMessage}</span></p>
+          })
+        }
+      </div>
+      <form className="appForm">
+        <input className="userNameInput" type="text" placeholder="User Name" onChange={this.onUserInputChange}/>
+        <input className="messageText" type="text" placeholder="Message Text" placeholder=" Message Text" onChange={this.onMessageChange}/>
+        <input className="submitMessage" type="submit" onClick={this.onSubmitMessage}/>
+        <input className="clearButton" type="button" value="clear" onClick={this.onMessagesClear}/>
+      </form>
+    </section>
+  }
+
+
+})
