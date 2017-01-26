f24b6ea9d676bcf6041c1d9fed14ddad6ee7a331
Daniel <dray364@gmail.com>
2017-01-26 17:50:27 -0600

    initialized npm/ added missing classes for project

--- 5.3-gift-react/js/main.js
+++ 5.3-gift-react/js/main.js
@@ -1,42 +1,53 @@
-// Grabbing react from node_modules
 import React from 'react'
 
 export default React.createClass({
   getInitialState: function() {
     return {
-      input: ''
+      inputValue: '',
+      priceValue: 0
     };
   },
-  onAddButtonClick(e) {
+
+  onADDButtonClick(e) {
+    // this should return the text from the text input and append a new list item
     e.preventDefault();
     var currentInputValue = this.state.inputValue;
+    var textInputArray = currentInputValue.split(",");
+    var listTextElement = textInputArray[0];
+    var priceElement = textInputArray[1];
+    this.refs.listItems.insertAdjacentHTML("afterbegin",
+    `<article class="listItem">
+      <button class="circleButton" type="button" value="line"></button>
+      <p class="listItem__text">${listTextElement}</p>
+      <p class="priceElement" ref="priceElement">$${priceElement}</p>
+      </article>`)
+
+    this.refs.totalPrice.innerHTML = priceElement;
   },
+
   updateInputValue: function(evt) {
     this.setState({
       inputValue: evt.target.value
     });
   },
 
-  render() {
+
+  render (){
     return (
-    <section className="section">
-      <h2 className= "h2" > Gifts To Buy </h2>
-      <main>
-        <form>
-          <input placeholder= "Enter Text Here" className= "giftInputText" onvalue={ this.state.inputValue } onChange={ this.updateInputValue } type="text" value={this.state.inputValue} ></input>
-          <input placeholder= "ADD" className= "addButton" type="submit" onClick={this.onADDButtonClick}></input>
-          <div className="listItems_div">
-            <article className="listItem"></article>
+      <main className="main">
+        <h2 className="h2">GIFTS TO BUY</h2>
+        <form className="items__form">
+          <input className="giftInputText" type="text" name="giftInput" placeholder="     name of gift, price" value={ this.state.inputValue } onChange={ this.updateInputValue }></input>
+          <input className="addButton" type="submit" name="submitItemToList" value="ADD" onClick={ this.onADDButtonClick }></input>
+          <div className="listItems__div" ref="listItems">
             <article className="totalAmount">
-              <p className="totalTextOnly">Total</p>
+              <p className="p totalTextOnly">Total</p>
               <p className="p">$</p>
-              <p className="p__totalCost">0</p>
+              <p className="p__totalCost" ref="totalPrice">0</p>
             </article>
           </div>
         </form>
       </main>
-    </section>
     )
   }
-
-});
+})
